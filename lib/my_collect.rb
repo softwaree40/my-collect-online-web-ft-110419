7b53d7560244092bbfbc6720839b72c29905d7de
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-06 05:47:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,19 +1,11 @@
 require 'pry'
 def my_collect(empty_array)
    new_collection = []
-   new_collection_first = []
-   
    i = 0 
    while i < empty_array.size
-      new_collection << yield(empty_array[i].upcase)
+      new_collection << empty_array[i].upcase)
       #binding.pry
    i+=1
   end
-     return new_collection
-      new_collection.each do |student|
-      students_first << student.first
-      binding.pry
-       
-    end
-    new_collection_first.downcase
+     
 end
