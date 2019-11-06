0f225627cf99805ede8fdb1125e24355300251ae
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-06 05:51:51 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,10 +3,10 @@ def my_collect(empty_array)
    new_collection = []
    i = 0 
    while i < empty_array.size
-      new_collection << yield(empty_array[i])
+      new_collection << yield(empty_array[i].upcase)
       
       #binding.pry
    i+=1
   end
-     
+   return new_collection  
 end
